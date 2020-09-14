492b96fde5276f13413dd68fc13c15d36dc8d1dd
ratnanil <rata@zhaw.ch>
2020-09-14 09:21:29 +0200

    commented script and changed if statement

--- show_hide_solution.py
+++ show_hide_solution.py
@@ -8,16 +8,19 @@ notebooks = glob("./*.ipynb", recursive=False) # recursive = True to search in s
 # Search through each notebook and look for the text, add a tag if necessary
 
 def musterloesung(notebooks,tag = "remove-cell"):
+    # for loops works through each ipynb-file
     for ipath in notebooks:
         ntbk = nbf.read(ipath, nbf.NO_CONVERT)
-
+        # for loop works through each cell of a given file
         for cell in ntbk.cells:
             cell_tags = cell.get('metadata', {}).get('tags', [])
+            # If the word "Musterlösung" contained in the cell content
             if "Musterlösung" in cell["source"]:
+                # removes all tags starting with "remove-" or "hide-"
                 cell_tags = [i for i in cell_tags if not re.match("remove|hide-.+",i)]
                 if tag != "": cell_tags.append(tag)    
-            if len(cell_tags) > 0:
-                cell['metadata']['tags'] = cell_tags
+                if len(cell_tags) > 0:
+                    cell['metadata']['tags'] = cell_tags
 
         nbf.write(ntbk, ipath)
         
